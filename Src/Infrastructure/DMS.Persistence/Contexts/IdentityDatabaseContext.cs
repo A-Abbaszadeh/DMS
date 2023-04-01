9f95ebc4edd9825e2082a7beda4ace213e44e4d1
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2023-04-01 14:22:46 +0330

    Create IdentityDatabaseContext and install some ef core packeges

---
+++ Src/Infrastructure/DMS.Persistence/Contexts/IdentityDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.Persistence.Contexts
+{
+    public class IdentityDatabaseContext:IdentityDbContext
+    {
+        public IdentityDatabaseContext(DbContextOptions<IdentityDatabaseContext> options) : base(options)
+        {
+            
+        }
+    }
+}
--- Src/Presentation/DMS.WebsiteApi.Endpoint/Program.cs
+++ Src/Presentation/DMS.WebsiteApi.Endpoint/Program.cs
@@ -1,4 +1,22 @@
+using DMS.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region Sql Server
+//services.AddTransient<IDatabaseContext, DatabaseContext>();
+
+string connectionString = builder.Configuration["ConnectionStrings:SqlServer"];
+builder.Services.AddDbContext<IdentityDatabaseContext>(option => option.UseSqlServer(connectionString));
+//services.AddDbContext<DatabaseContext>(option => option.UseSqlServer(connectionString));
+
+//services.AddDistributedSqlServerCache(option =>
+//{
+//    option.ConnectionString = connectionString;
+//    option.SchemaName = "dbo";
+//    option.TableName = "CacheData";
+//});
+#endregion
 
 // Add services to the container.
 
